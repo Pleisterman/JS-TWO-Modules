afbb73b97e0496dbe11f94480a23edd4b9b2747e
Pleisterman <info@pleisterman.nl>
2015-03-12 18:28:22 +0100

    Merge origin/master
    Conflicts:
    	jsProject/jsProject.js

---
+++ README.md
@@ -0,0 +1,20 @@
+# JS-SingleModule
+
+An example of javascript modules
+
+this is an example of two javascript modules.
+
+it is only for educational purposes.
+
+The module describes the basic code for creating 
+javascript modules according to the revealing module pattern.
+
+This file belongs to a series that describes the use of the
+Revealing Module Pattern in a complex javascript web project.
+
+The resulting javascript project can be found on Github under
+Pleisterman - jsProject.
+
+for more info:
+
+website: www.pleisterman.nl/en/
---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Single Javascript Module</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+<script type="text/javascript">
+( function() {
+
+    window.mainModule = new function(){};
+
+    // private 
+    var self = window.mainModule;
+    self.MODULE = 'mainModule';
+    self.helloWorld = null;
+
+    self.construct = function() {
+    };
+    self.sayHello = function() {
+        if( !self.helloWorld ){
+            self.helloWorld = new mainModule.helloWorldModule();
+        }
+        self.helloWorld.sayHello();
+    };
+    self.construct();
+    // public
+    return {
+        sayHello : function() {
+            self.sayHello();
+        }
+    };
+})();
+
+( function( mainModule ){
+    mainModule.helloWorldModule = function( ) {
+
+        // private
+        var self = this;
+        self.MODULE = 'helloWorldModule';
+        // functions
+        self.construct = function() {
+        };
+        self.sayHello = function() {
+            alert( 'hello' );
+        };
+
+        self.construct();
+
+        // public
+        return {
+            sayHello : function(){
+                self.sayHello();
+            }
+        };
+    };
+})( mainModule );
+
+window.onload = function() {
+    mainModule.sayHello(); 
+}
+</script>    
+    
+    </head>
+    <body>
+    </body>
+</html>
